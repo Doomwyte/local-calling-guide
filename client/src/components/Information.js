9faef80ac288744ca78cf255534592f9872c888f
Doomwyte <doomwyte@gmail.com>
2018-04-20 16:19:25 -0700

    working on link routing

--- client/src/components/Information.js
+++ client/src/components/Information.js
@@ -1,75 +1,71 @@
 import React from "react";
-import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+import List from "./List";
 
 class Information extends React.Component{
 	render(){
 		return(
-			<Router>
-				<div className="number__info">
-					{
-						this.props.npanxx &&
-						<p className="number__key">NPA-NXX:
-							<span className="number__value"> {this.props.npanxx}</span>
-						</p>
-					}
-					{ 
-						this.props.rateCenter &&
-						<p className="number__key">Rate Center: 
-								<span className="number__value"> {this.props.rateCenter}</span>
-						</p> 
-					}
-					{ 
-						this.props.province && 
-						<p className="number__key">Province: 
-							<Link to={"/list"}>
-								<span className="number__value"> {this.props.province}</span>
-							</Link>
-						</p> 
-					}
-					{ 
-						this.props.switch && 
-						<p className="number__key">Switch: 
-							<Link to={"/list"}>
-								<span className="number__value"> {this.props.switch}</span>
-							</Link>
-						</p> 
-					}
-					{ 
-						this.props.ocn && 
-						<p className="number__key">OCN: 
-							<Link to={"/list"}>
-								<span className="number__value"> {this.props.ocn}</span>
-							</Link>
-						</p> 
-					}				
-					{ 
-						this.props.companyName && 
-						<p className="number__key">Company Name: 
-							<Link to={"/list"}>
-								<span className="number__value"> {this.props.companyName}</span>
-							</Link>
-						</p> 
-					}	
-					{ 
-						this.props.portability && 
-						<p className="number__key">Portability: 
-							<span className="number__value"> {this.props.portability}</span>
-						</p> 
-					}			
-					{ 
-						this.props.lrn && 
-						<p className="number__key">LRN: 
-							<span className="number__value"> {this.props.lrn}</span>
-						</p> 
-					}								
-					{ 
-						this.props.error && <p className="number__error">{ this.props.error }</p> 
-					}
-				</div>
-				{/* <div>
-					<Route path="/list" component={List}/>
-				</div> */}
-			</Router>
+			<div className="number__info">
+				{
+					this.props.npanxx &&
+					<p className="number__key">NPA-NXX:
+						<span className="number__value"> {this.props.npanxx}</span>
+					</p>
+				}
+				{ 
+					this.props.rateCenter &&
+					<p className="number__key">Rate Center: 
+							<span className="number__value"> {this.props.rateCenter}</span>
+					</p> 
+				}
+				{ 
+					this.props.province && 
+					<p className="number__key">Province: 
+						<Link to={"/list"}>
+							<span className="number__value"> {this.props.province}</span>
+						</Link>
+					</p> 
+				}
+				{ 
+					this.props.switch && 
+					<p className="number__key">Switch: 
+						<Link to={"/list"}>
+							<span className="number__value"> {this.props.switch}</span>
+						</Link>
+					</p> 
+				}
+				{ 
+					this.props.ocn && 
+					<p className="number__key">OCN: 
+						<Link to={"/list"}>
+							<span className="number__value"> {this.props.ocn}</span>
+						</Link>
+					</p> 
+				}				
+				{ 
+					this.props.companyName && 
+					<p className="number__key">Company Name: 
+						<Link to={"/list"}>
+							<span className="number__value"> {this.props.companyName}</span>
+						</Link>
+					</p> 
+				}	
+				{ 
+					this.props.portability && 
+					<p className="number__key">Portability: 
+						<span className="number__value"> {this.props.portability}</span>
+					</p> 
+				}			
+				{ 
+					this.props.lrn && 
+					<p className="number__key">LRN: 
+						<span className="number__value"> {this.props.lrn}</span>
+					</p> 
+				}								
+				{ 
+					this.props.error && <p className="number__error">{ this.props.error }</p> 
+				}
+			</div>
 		);
 	}
 };
